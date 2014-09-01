876adef8cc013c2ca07b9cb758fe28cd265521ec
dmwilliams.tech@gmail.com <dmwilliams.tech@gmail.com>
2014-09-01 17:42:37 -0400

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe PagesController, :type => :controller do
 
+  render_views
+
   describe "GET home" do
     it "returns http success" do
       get :home
       expect(response).to be_success
+    end
+    it "should have the right title" do
+      get :home
+      expect(response).to have_selector("title")#, :content => "Ruby on Rails Tutrial Sample App | Home")
     end
   end
 
@@ -14,6 +20,25 @@ RSpec.describe PagesController, :type => :controller do
       get :contact
       expect(response).to be_success
     end
+    # it "should have the right title" do
+    #   get :contact
+    #   expect(response).should have_selector("title")#, :content => "Ruby on Rails Tutrial Sample App | Contact")
+    # end
+
   end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get :about
+      response.should be_success
+    end
+    # it "should have the right title" do
+    #   get :about
+    #   expect(response).should have_selector("title")#, :content => "Ruby on Rails Tutrial Sample App | About")
+    # end
+
+  end
+
+
 
 end
